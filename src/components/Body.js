c96262470d3b3d20996c330145cac3000f250dac
anand <anandpromax@gmail.com>
2024-02-04 23:54:31 +0530

    Update dependencies and remove unused code

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,16 +2,13 @@ import React, { useEffect } from 'react'
 import Login from './Login'
 import Browse from './Browse'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import Home from './Home'
+ 
 
 function Body() {
     const appRouter = createBrowserRouter([
+         
         {
             path: '/',
-            element: <Home />,
-        },
-        {
-            path: '/login',
             element: <Login />,
         },
         {
